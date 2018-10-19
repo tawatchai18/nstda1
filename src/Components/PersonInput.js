c07e3074cac6d507fe3c6e328b1eca5d96105b2a
account <tawatchai@codefin.io>
2018-10-19 10:46:53 +0700

    no message

---
+++ src/Components/PersonInput.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import axios from 'axios';
+
+export default class PersonInput extends React.Component {
+  state = {
+    name: '',
+  };
+
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+      })
+  }
+
+  render() {
+    return (
+      <ul>
+        { this.state.persons.map(person => <li>{person.name}</li>)}
+      </ul>
+    )
+  }
+}
